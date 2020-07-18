374909f6925c252f65dc8bae35ef731e71615e3e
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-07-18 22:32:09 +0300

    first div on form deleted

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ function myDate() {
   var date = document.getElementById("date").value; 
   var gender1 = document.getElementById("male").value;
   var gender2 = document.getElementById("female").value
-  //alert( date).onsubmit; 
+  //alert(date).onsubmit; 
 
-//if(date === "12-12-2020" && gender === "male"){
- // alert("yaay").onsubmit;
+if(date === "12-12-2020" && gender === "male"){
+ alert("yaay").onsubmit;
 //}
 //else{
   //alert("na-ah").onsubmit
 //}
 
-if(gender1 ==="male" && date === "12-12-2020"){
-  alert("male")
-}
-else{
-  alert('not male')
-}
+//if(gender1 ==="male" && date === "12-12-2020"){
+ // alert("male")
+//}
+//else{
+  //alert('not male')
+//}
 }
 
