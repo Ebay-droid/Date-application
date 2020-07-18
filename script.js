11b2876b860f577926490c7efda03930ca9cebb6
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-07-18 16:01:32 +0300

    divs added in html

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function myDate() {
   var date = document.getElementById("date").value; 
   //alert( date).onsubmit; 
 
-if(date === "12-12-2020" && "male"){
-  alert(yaay).onsubmit;
+if(date === "12-12-2020" ){
+  alert("yaay").onsubmit;
 }
 else{
-  alert(na-ah).onsubmit
+  alert("na-ah").onsubmit
 }
 
 }
