4e5a9a03788232862738758cf304ea4166b3a864
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-07-18 23:16:29 +0300

    READme file added

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function myDate() {
   var date = document.getElementById("date").value; 
-  var gender1 = document.getElementById("male").value;
-  var gender2 = document.getElementById("female").value
+  //var gender1 = document.getElementById("male").value;
+  //var gender2 = document.getElementById("female").value
   //alert(date).onsubmit; 
+  var gender = document.getElementById ("male", "female")
+  alert(date + gender).onsubmit;
 
-if(date === "12-12-2020" && gender === "male"){
- alert("yaay").onsubmit;
+
+//if(date === "12-12-2020" && gender === "male"){
+ //alert("yaay").onsubmit;
 //}
 //else{
-  //alert("na-ah").onsubmit
+//alert("na-ah").onsubmit
 //}
 
 //if(gender1 ==="male" && date === "12-12-2020"){
