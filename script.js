2e5b6dbebe95ca3be3f32c0aff0dff999cc2f0ee
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-07-18 21:28:43 +0300

    js update

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 function myDate() {
   var date = document.getElementById("date").value; 
+  var gender1 = document.getElementById("male").value;
+  var gender2 = document.getElementById("female").value
   //alert( date).onsubmit; 
 
-if(date === "12-12-2020" ){
-  alert("yaay").onsubmit;
+//if(date === "12-12-2020" && gender === "male"){
+ // alert("yaay").onsubmit;
+//}
+//else{
+  //alert("na-ah").onsubmit
+//}
+
+if(gender1 ==="male" && date === "12-12-2020"){
+  alert("male")
 }
 else{
-  alert("na-ah").onsubmit
+  alert('not male')
 }
-
 }
-
 
