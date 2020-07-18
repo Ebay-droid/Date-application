4648210a044ca628d2cf8668e6200fe5c9c2b599
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-07-18 08:47:38 +0300

    css file added

---
+++ script.js
@@ -0,0 +1,11 @@
+function myDate() {
+  var day = document.getElementById("day".value);
+  var month = document.getElementById("month".value);
+  var year = document.getElementById("year".value);
+
+
+  alert( day+month+year).onsubmit;
+ 
+}
+
+
