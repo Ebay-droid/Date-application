6d09b93335b201f976f7efd9652106d46319dace
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-07-19 12:21:25 +0300

    getDay method applied

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 function myDate() {
   var date = document.getElementById("date").value; 
   var gender = document.getElementById("gender").value
-  alert(date + ',' + gender).onsubmit;
+  //alert(date + ',' + gender).onsubmit;
+  //var myDay = date.getDay()
+  //alert(myDay.getDay).onsubmit;
+  var myDay = new Date(date);
+  //console.log(myDay.getDay)
 
 
-//if(date === "12-12-2020" && gender === "male"){
- //alert("yaay").onsubmit;
-//}
-//else{
-//alert("na-ah").onsubmit
-//}
 
-//if(gender1 ==="male" && date === "12-12-2020"){
- // alert("male")
-//}
-//else{
-  //alert('not male')
-//}
+   alert(date + ','+ ' ' + myDay.getDay() + ',' + ' ' + gender).onsubmit
+
+
+
+
+
 }
 
